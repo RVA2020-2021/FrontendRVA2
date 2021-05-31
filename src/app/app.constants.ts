7e090230b67c162d9bdb56daa8be06c3d84964de
jelenapetrovacki <jpetrovacki3@gmail.com>
2021-05-31 12:35:45 +0200

    Endpoint-i i _redirects fajl

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,6 +1,14 @@
+/*
 export const ARTIKL_URL= 'http://localhost:8083/artikl';
 export const DOBAVLJAC_URL= 'http://localhost:8083/dobavljac';
 export const PORUDZBINA_URL= 'http://localhost:8083/porudzbina';
 export const STAVKE_ZA_PORUDZBINU_URL= 'http://localhost:8083/stavkeZaPorudzbinaID';
 export const STAVKE_PORUDZBINE_URL= 'http://localhost:8083/stavkaPorudzbine';
+*/
 
+
+export const ARTIKL_URL= 'https://rva-backend-gr2.herokuapp.com/artikl';
+export const DOBAVLJAC_URL= 'https://rva-backend-gr2.herokuapp.com/dobavljac';
+export const PORUDZBINA_URL= 'https://rva-backend-gr2.herokuapp.com/porudzbina';
+export const STAVKE_ZA_PORUDZBINU_URL= 'https://rva-backend-gr2.herokuapp.com/stavkeZaPorudzbinaID';
+export const STAVKE_PORUDZBINE_URL= 'https://rva-backend-gr2.herokuapp.com/stavkaPorudzbine';
